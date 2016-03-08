ee2798f8861b65e1940b93fea0172eb35d6ee544
Paul Martin <codemobius@gmail.com>
2016-03-07 23:21:39 -0500

    updated chat client for pgp and presence

--- mchat.go
+++ mchat.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/MobiusHorizons/go_mqtt_chat"
+	"golang.org/x/crypto/ssh/terminal"
 	"io"
 	"os"
 	"strings"
@@ -15,20 +16,41 @@ func main() {
 	server := flag.String("server", "mqtt://test.mosquitto.org:1883", "server to connect to")
 	flag.Parse()
 
-	fmt.Println(flag.Args())
 	if flag.NArg() < 1 {
 		fmt.Fprintf(os.Stderr, "Usage: %s --nick <nickname> [--server mqtt(s)://server.tld:port ] recipient\n", os.Args[0])
 		os.Exit(1)
 	}
 
+	fmt.Print("Password to unlock pgp key: ")
+	password, err := terminal.ReadPassword(0)
+	fmt.Println("")
+	for err != nil {
+		fmt.Println(err.Error())
+		fmt.Print("Password to unlock pgp key: ")
+		password, err = terminal.ReadPassword(0)
+		fmt.Println("")
+	}
+
 	recipient := flag.Arg(0)
 
 	//	fmt.Println("nick = ", *nick, "server = ", *server)
-	client := go_mqtt_chat.New(*nick, *server, nil)
-	err := client.Connect()
+	client := go_mqtt_chat.New(*nick, *server, string(password), nil)
+	err = client.Connect()
 	if err != nil {
 		panic(err)
 	}
+
+	go func() {
+		m := client.Presence()
+		for m != nil {
+			fmt.Println(m.From + ": " + m.Message)
+			if m.Message == "online" {
+				fmt.Println("adding user '" + m.From + "'")
+				client.Meet(m.From)
+			}
+			m = client.Presence()
+		}
+	}()
 
 	go func() {
 		m := client.Listen()
